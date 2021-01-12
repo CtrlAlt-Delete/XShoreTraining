aa6d675bbfddeaf79ae8891c2912f9ad04565004
CtrlAlt-Delete <jonathan.pernow@gmail.com>
2021-01-12 15:45:15 +0100

    Print comma in function

---
+++ CTraining/Chapter2_ProgramStructure/hello2Function.c
@@ -0,0 +1,24 @@
+/* 
+ * hello2Function.c
+ * print the comma in a function.
+ * by Jonathan Pernow
+ * created 2021-01-12
+*/
+
+#include <stdio.h>
+
+void printComma()
+{
+    printf(", ");
+    return;
+}
+
+int main()
+{
+    printf( "Hello" );
+    printComma();
+    printf( "world!\n" );
+    return 0;
+}
+
+/* EOF */
