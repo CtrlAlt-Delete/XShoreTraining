2bd4617329b7e654fac84e40168a8ed2ee443ed0
CtrlAlt-Delete <jonathan.pernow@gmail.com>
2021-01-12 11:15:43 +0100

    hello2.c

---
+++ CTraining/Chapter1_HelloWorld/hello2.c
@@ -0,0 +1,15 @@
+/* 
+ * hello2.c
+ * First C program with comments.
+ * by Jonathan Pernow
+ * created 2021-01-12
+ */
+
+#include <stdio.h>
+int main()
+{
+printf( "Hello, world!\n" );
+return 0;
+}
+
+/* eof */
