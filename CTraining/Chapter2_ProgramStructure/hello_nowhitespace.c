f0a111d8070d3a83860957e1dbfcc4d71ebdb674
CtrlAlt-Delete <jonathan.pernow@gmail.com>
2021-01-12 15:15:32 +0100

    removed whitespaces

---
+++ CTraining/Chapter2_ProgramStructure/hello_nowhitespace.c
@@ -0,0 +1,11 @@
+/* 
+ * hello_nowhitespace.c
+ * program with no white spaces.
+ * by Jonathan Pernow
+ * created 2021-01-12
+*/
+
+#include<stdio.h>
+int main(){printf( "Hello, world!\n" );return 0;}
+
+/* EOF */
